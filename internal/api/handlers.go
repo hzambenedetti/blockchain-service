001c44ce0f01634dfc57b1703eee0c2f88f21cea
hzambenedetti <hique.rz03@gmail.com>
2025-05-22 16:51:49 -0300

    feat: Separated file opening logic in it's own function

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -1,10 +1,12 @@
 package api
 
 import (
-    "blockchain-service/internal/blockchain"
-    "github.com/gofiber/fiber/v2"
-    "github.com/gofiber/fiber/v2/log"
-    "crypto/sha256" 
+	"blockchain-service/internal/blockchain"
+	"crypto/sha256"
+	"mime/multipart"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
 )
 
 type PDFHandler struct {
@@ -17,25 +19,17 @@ func (h *PDFHandler) UploadPDF(c *fiber.Ctx) error{
     log.Error("Failed to Generate file from ")
     return c.Status(400).JSON(fiber.Map{"error": err.Error()})
   }
-
-  fileHandler, err  := fileHeader.Open()
-  if err != nil{
-    log.Error("Failed to open file")
-    return c.Status(500).JSON(fiber.Map{"error": err.Error()})
-  }
   
-  buffer := make([]byte, fileHeader.Size)
-  bytesRead , err := fileHandler.Read(buffer[:])
+  buffer, err := getFileContents(fileHeader)
   if err != nil{
     log.Error("Failed to read file contents")
     return c.Status(500).JSON(fiber.Map{"error": err.Error()})
   }
-  log.Info("Read %d bytes from %s", bytesRead, fileHeader.Filename)
 
   fileHash := sha256.Sum256(buffer) 
   txHash, err := h.EthClient.StoreHash(string(fileHash[:]))
   if err != nil{
-    log.Error("failed")
+    log.Error("Failed to store Hash on the database")
   }
 
   //Store transaction details in the database 
@@ -46,4 +40,24 @@ func (h *PDFHandler) UploadPDF(c *fiber.Ctx) error{
 func (h *PDFHandler) VerifyPDF(c *fiber.Ctx) error{
 	//TODO
 	return nil
+}
+
+func getFileContents(header *multipart.FileHeader) ([]byte, error){
+  fileHandler, err  := header.Open()
+  defer fileHandler.Close()
+
+  if err != nil{
+    log.Error("Failed to open file")
+    return nil, err
+  }
+  
+  buffer := make([]byte, header.Size)
+  bytesRead , err := fileHandler.Read(buffer[:])
+  if err != nil{
+    log.Error("Failed to read file contents")
+    return nil, err
+  }
+  log.Info("Read %d bytes from %s", bytesRead, header.Filename)
+
+  return buffer, nil
 }
