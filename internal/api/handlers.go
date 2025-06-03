2851e68e143f1ff9f982cebdb4eeef2aab21dd1f
hzambenedetti <hique.rz03@gmail.com>
2025-06-02 22:54:12 -0300

    feat: Implemented API version for blocks and block data

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -1,7 +1,7 @@
 package api
 
 import (
-	"blockchain-service/internal/blockchain"
+	"blockchain-service/internal/models"
 	"blockchain-service/internal/p2p"
 
 	"github.com/gofiber/fiber/v2"
@@ -12,18 +12,25 @@ type NodeAPIHandler struct {
 }
 
 func (h *NodeAPIHandler) UploadHash(c *fiber.Ctx) error{
-	var blockData blockchain.BlockData 
-	if err := c.BodyParser(blockData); err != nil{
-		return c.SendStatus(401)
+	var blockDataAPI models.BlockDataAPI 
+	if err := c.BodyParser(blockDataAPI); err != nil{
+		return c.SendStatus(fiber.ErrBadRequest.Code)
+	}
+	
+	blockData, err := blockDataAPI.ToBlockData()
+	if err != nil{
+		return c.SendStatus(fiber.ErrBadRequest.Code)
 	}
 
-	block, err := h.Node.AddBlockAPI(&blockData)
+	block, err := h.Node.AddBlockAPI(blockData)
 
 	if err != nil{
 		return c.SendStatus(500)
 	}
 
-	return c.Status(201).JSON(block)
+	blockAPI := models.FromBlock(block)
+
+	return c.Status(201).JSON(blockAPI)
 }
 
 func (h *NodeAPIHandler) VerifyHash(c *fiber.Ctx) error{
